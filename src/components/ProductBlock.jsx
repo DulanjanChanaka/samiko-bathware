dd0ea1af9d755a1046ab536b2ad6b962aa8752cf
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-08-01 17:53:21 +0530

    first commit

--- src/components/ProductBlock.jsx
+++ src/components/ProductBlock.jsx
@@ -1,5 +1,4 @@
 "use client"
-import { styled } from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -12,7 +11,7 @@ import { Navigation } from "swiper/modules";
 
 
 
-const ProductBlock = ({ title, products = [] }) => {
+const ProductBlock = ({ title= [] }) => {
     const productlist = [
         {
           title: "product title 1",
@@ -117,7 +116,7 @@ const ProductBlock = ({ title, products = [] }) => {
         modules={[Navigation]}
       >
         {productlist?.map(
-          ({ title, src, alt, price, discount, }, index) => (
+          ({ title, src, alt, price, }, index) => (
             <SwiperSlide key={index}>
               <Product
                 title={title}
@@ -141,7 +140,7 @@ export default ProductBlock;
  *
  */
 
-const Product = ({ title, src, alt, price, discount,  }) => {
+const Product = ({ src, alt, price, discount,  }) => {
   return (
     <div className="  top-0 left-0 pb-3 bg-black p-[3px 7px] font-medium text-white rounded-md hover:scale-105 ease-in duration-300">
       <img className="w-[100%] object-contain" src={src} alt={alt} />
@@ -155,47 +154,3 @@ const Product = ({ title, src, alt, price, discount,  }) => {
   );
 };
 
-/**
- *
- */
-
-const HProductBlockContainer = styled.div`
-  width: 100%;
-  margin-bottom: 30px;
-  
-  
-`;
-const ProductsTitle = styled.h2``;
-
-const ProductContainer = styled.div`
-  width: 100%;
-  position: relative;
-  box-shadow: rgba(0, 0, 0, 0.24)  ;
-  margin: 12px 0;
-  border-radius:20px;
-  
-
-  img {
-    width: 100%;
-    object-fit: contain;
-  }
-`;
-
-const ProductDiscout = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: black;
-  color: white;
-  padding: 3px 7px;
-  font-size: 10px;
-  font-weight: 600;
-`;
-
-const ProductDetails = styled.div`
-  padding: 5px;
-
-  p {
-    font-size: 13px;
-  }
-`;

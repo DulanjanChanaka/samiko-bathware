9f3ee601962148c4972ac1cdd77a64f054c13ac5
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-08-01 19:17:44 +0530

    first commit

--- src/components/ProductBlock.jsx
+++ src/components/ProductBlock.jsx
@@ -104,28 +104,31 @@ const ProductBlock = ({ title= [] }) => {
       ];
       
 
-  //     const [slidesPerView, setSlidesPerView] = useState(4);
+      const [slidesPerView, setSlidesPerView] = useState(4);
 
-  // useEffect(() => {
-  //   // Update the slidesPerView based on window width on the client-side
-  //   function handleResize() {
-  //     setSlidesPerView(innerWidth < 600 ? 2 : 4);
-  //   }
+  useEffect(() => {
+    // Update the slidesPerView based on window width on the client-side
+    function handleResize() {
+      setSlidesPerView(window.innerWidth < 600 ? 2 : 4);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    // Call the handleResize function initially to set the initial slidesPerView
+    handleResize();
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);}
 
 
-  // }, []);
+  }, []);
 
   return (
-    <div className="w-[100%] mb-8 ">
-    <h2>{title}</h2>
-    <Swiper
-      slidesPerView={
-        innerWidth < 600 ? 2 :  4
-      }
-      spaceBetween={10}
-      navigation={true}
-      modules={[Navigation]}
-    >
+    <div className="w-full mb-8">
+      <h2>{title}</h2>
+      {/* Render the Swiper directly without checking typeof window */}
+      <Swiper slidesPerView={slidesPerView} spaceBetween={10} navigation modules={[Navigation]}>
         {productlist?.map(({ title, src, alt, price }, index) => (
           <SwiperSlide key={index}>
             <Product title={title} src={src} alt={alt} price={price} />

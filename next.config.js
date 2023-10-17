8f949b3a2f730c853e825ef13ba0464dcc0b66d5
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-10-17 22:09:53 +0530

    next config change

--- next.config.js
+++ next.config.js
@@ -3,24 +3,7 @@ const nextConfig = {
     images: {
       domains: ["firebasestorage.googleapis.com"],
     },
-    theme: {
-      screens: {
-        'sm': '700px',
-        // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-      }
-    }
+    
   };
   
   module.exports = nextConfig;

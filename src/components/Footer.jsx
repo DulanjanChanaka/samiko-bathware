f2ef6744a1b03a149ae2f9e5a957b6b415fd6e76
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-10-19 23:08:03 +0530

    add fb icon, location

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='m-0 bottom-0 bg-black w-full text-white pt-5 pb-6'>
+    <div className='m-0 bottom-0 bg-black w-full text-white pt-5 pb-6 mt-16'>
        <div className='text-center pb-3'>
         <h2>SAMIKO</h2>
       </div>
